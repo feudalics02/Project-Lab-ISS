7fc4c6c3834b2003aadeee5a9359223d09182ac4
Dragos <dragosjulean@gmail.com>
2023-05-07 17:39:31 +0300

    First Iteration

--- Application/src/main/java/lab/iss/controllers/PharmacistController.java
+++ Application/src/main/java/lab/iss/controllers/PharmacistController.java
@@ -5,17 +5,13 @@ import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import lab.iss.business.Service;
 import lab.iss.domain.Order;
 import lab.iss.domain.Pharmacist;
 
-import java.io.IOException;
 import java.util.List;
 
 public class PharmacistController {
